7752f5835416cf0415adf6ad8c4ab24fffc7769c
alexx2203 <alex2203x@gmail.com>
2025-01-04 14:13:19 +0100

    Added all interfaces

---
+++ Animal_Simulation/enum/View.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_Simulation
+{
+    public enum View
+    {
+        Tab = 1,
+        List = 2
+    }
+
+
+}
---
+++ Animal_Simulation/repository/IRun.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_Simulation.repository
+{
+    public interface IRun
+    {
+        void Run(int energyLevel);
+    }
+}
